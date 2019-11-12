35704627a323899cc907e3449cea7cf992f13b1e
rahuldevatgit <email.rahuldev@gmail.com>
2019-11-13 01:41:35 +0530

    user story 1

--- assignment/app.py
+++ assignment/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-​
+
 @app.route("/getCustomerCharge", methods=["POST"])
 def getCustomerCharge():
     mydata = request.get_json()
