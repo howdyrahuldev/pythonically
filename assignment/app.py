c2273fa63513b6994fcc964607f159c12a6735c5
rahuldevatgit <email.rahuldev@gmail.com>
2019-11-13 02:03:01 +0530

    user story 2

--- assignment/app.py
+++ assignment/app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route("/getCustomerCharge", methods=["POST"])
 def getCustomerCharge():
     mydata = request.get_json()
-    return jsonify({"Total Price":f'{mydata["noOfBooks"]*mydata["noOfDays"]} Rs'})
+    book_price = {"regular":1.5,"fiction":3,"novels":1.5}
+    total_price=0
+    for item in mydata:
+        total_price += mydata[item]["noOfBooks"]*mydata[item]["noOfDays"]*book_price[mydata[item]["typeOfBooks"]]
+    return jsonify({"Total Price":f'{total_price} Rs'})
+
 
 app.run(port=5050)
--- assignment/test_app.py
+++ assignment/test_app.py
@@ -4,11 +4,13 @@ from flask import json
 def test_getCustomerCharge():
     response = app.test_client().post(
         '/getCustomerCharge',
-        data=json.dumps({"noOfBooks":10,	"noOfDays":3}),
+        data=json.dumps({   "1":{"typeOfBooks":"fiction","noOfBooks":1,"noOfDays":10},
+	                        "2":{"typeOfBooks":"regular","noOfBooks":3,"noOfDays":10},
+	                        "3":{"typeOfBooks":"novels","noOfBooks":1,"noOfDays":30}}),
         content_type='application/json',
     )
 
     data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 200
-    assert data['Total Price'] == "30 Rs"
+    assert data['Total Price'] == "120.0 Rs"
