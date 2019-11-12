d087f8fba57fe0080559decd53955a658ab91c18
rahuldevatgit <email.rahuldev@gmail.com>
2019-11-13 02:26:02 +0530

    user story 3

--- assignment/app.py
+++ assignment/app.py
@@ -8,7 +8,25 @@ def getCustomerCharge():
     book_price = {"regular":1.5,"fiction":3,"novels":1.5}
     total_price=0
     for item in mydata:
-        total_price += mydata[item]["noOfBooks"]*mydata[item]["noOfDays"]*book_price[mydata[item]["typeOfBooks"]]
+        if mydata[item]["typeOfBooks"] == "regular":
+            if mydata[item]["noOfDays"] == 1:
+                total_price += mydata[item]["noOfBooks"]*2
+            elif mydata[item]["noOfDays"] >= 2:
+                part_price = 1
+                total_price += mydata[item]["noOfBooks"] * book_price[
+                    mydata[item]["typeOfBooks"]] * (mydata[item]["noOfDays"]-2) + mydata[item]["noOfBooks"]*2*part_price
+
+
+        elif mydata[item]["typeOfBooks"] == "novels":
+            if 0 < mydata[item]["noOfDays"] < 3:
+                total_price += mydata[item]["noOfBooks"]*4.5
+            elif mydata[item]["noOfDays"] >= 3:
+                total_price += mydata[item]["noOfBooks"]*mydata[item]["noOfDays"]*book_price[mydata[item]["typeOfBooks"]]
+
+
+        else:
+            total_price += mydata[item]["noOfBooks"] * mydata[item]["noOfDays"] * book_price[mydata[item]["typeOfBooks"]]
+
     return jsonify({"Total Price":f'{total_price} Rs'})
 
 
--- assignment/test_app.py
+++ assignment/test_app.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import json
 
-def test_getCustomerCharge():
+def test_getCustomerCharge1():
     response = app.test_client().post(
         '/getCustomerCharge',
         data=json.dumps({   "1":{"typeOfBooks":"fiction","noOfBooks":1,"noOfDays":10},
@@ -13,4 +13,33 @@ def test_getCustomerCharge():
     data = json.loads(response.get_data(as_text=True))
 
     assert response.status_code == 200
-    assert data['Total Price'] == "120.0 Rs"
+    assert data['Total Price'] == "117.0 Rs"
+
+def test_getCustomerCharge2():
+    response = app.test_client().post(
+        '/getCustomerCharge',
+        data=json.dumps({   "1":{"typeOfBooks":"fiction","noOfBooks":1,"noOfDays":10},
+	                        "2":{"typeOfBooks":"regular","noOfBooks":1,"noOfDays":1},
+	                        "3":{"typeOfBooks":"novels","noOfBooks":1,"noOfDays":30}}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['Total Price'] == "77.0 Rs"
+
+
+def test_getCustomerCharge3():
+    response = app.test_client().post(
+        '/getCustomerCharge',
+        data=json.dumps({   "1":{"typeOfBooks":"fiction","noOfBooks":1,"noOfDays":10},
+	                        "2":{"typeOfBooks":"regular","noOfBooks":1,"noOfDays":1},
+	                        "3":{"typeOfBooks":"novels","noOfBooks":1,"noOfDays":2}}),
+        content_type='application/json',
+    )
+
+    data = json.loads(response.get_data(as_text=True))
+
+    assert response.status_code == 200
+    assert data['Total Price'] == "36.5 Rs"
